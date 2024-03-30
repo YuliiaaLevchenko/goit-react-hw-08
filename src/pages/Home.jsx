9f645a0104bc584e9c315ff2f1bd514493cf11fd
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-03-30 20:18:37 +0200

    token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
+
 import PageTitle from "../components/PageTitle/PageTitle";
-import { fetchContacts } from "../redux/contacts/operations";
-import { selectError, selectLoading } from "../redux/contacts/slice";
-import { useEffect } from "react";
+
 import ContactForm from "../components/ContactForm/ContactForm";
-import Loader from "../components/Loader/Loader";
-import Error from "../components/Error/Error";
+
 import SearchBox from "../components/SearchBox/SearchBox";
 import ContactList from "../components/ContactList/ContactList";
 import { Toaster } from "react-hot-toast";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
+  
   return (
     <div>
        <PageTitle>
     
     <ContactForm />
-    {error && <Error></Error>}
-  {loading && <Loader>Loading message</Loader>}
+    
     <SearchBox />
     <ContactList />
     <Toaster />
